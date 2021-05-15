2e2c09cf3e63358047221ad91e457d822a450d00
Your Name <pinzhangchen@outlook.com>
2021-05-16 00:28:57 +0200

    adding Spring Config. Closes #22

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 // hard class OwnerServiceMap extends Abstrac and implement C rudservice
 public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService{
 
